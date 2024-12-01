1eb07e66f69f99c742d28c5141d61935f8247b2a
Maria-Teodora Popescu <popescumariateodora@yahoo.com>
2024-12-01 16:16:19 +0200

    Update Laborator3.sql

--- scripts/pl_sql/Laborator3.sql
+++ scripts/pl_sql/Laborator3.sql
@@ -1,202 +1,268 @@
-SET serveroutput ON;
-
-DECLARE
-    CURSOR c_angajati IS
-        SELECT 
-            d.department_name, 
-            e.employee_id,
-            e.first_name || ' ' || e.last_name AS nume, 
-            e.salary, 
-            e.commission_pct
-        FROM
-            employees e
-            INNER JOIN departments d
-            ON e.department_id = d.department_id
-        WHERE
-            UPPER(e.job_id) LIKE '%MGR' OR
-            UPPER(e.job_id) LIKE '%MAN' OR
-            UPPER(e.job_id) LIKE '%PRES';
-    
-    angajat c_angajati%ROWTYPE;
-    venit NUMBER;
-BEGIN
-    OPEN c_angajati;
-    
-    -- Output the headers
-    DBMS_OUTPUT.put_line(
-        RPAD('ECUSON', 10) ||
-        RPAD('NUME', 30) ||
-        RPAD('DEPARTAMENT', 20) ||
-        RPAD('VENIT', 10)
-    );
-    
-    DBMS_OUTPUT.put_line(
-        RPAD('=', 10, '=') ||
-        RPAD('=', 30, '=') ||
-        RPAD('=', 20, '=') ||
-        RPAD('=', 10, '=')
-    );
-
-    -- Loop through the cursor
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
-
-        -- Calculate the income (venit)
-        venit := ROUND(angajat.salary + NVL(angajat.commission_pct, 0) * angajat.salary);
-
-        -- Output the values
-        DBMS_OUTPUT.put_line(
-            RPAD(angajat.employee_id, 10) ||
-            RPAD(angajat.nume, 30) ||
-            RPAD(angajat.department_name, 20) ||
-            RPAD(venit, 10)
-        );
-    END LOOP;
-
-    -- Close the cursor
-    CLOSE c_angajati;
-END;
+-- Exercitiul 1
+-- id dep de la tastatura si se afiseaza nume_dep
+set serveroutput on
+declare
+    id_dep number;
+    nume_dep departments.department_name%type;
+begin
+    id_dep := &iddep;
+    select department_name into nume_dep from departments where department_id = id_dep;
+    dbms_output.put_line(id_dep||' - '||nume_dep);
+end;
 /
 
-
--- Ex. 8 Să se modifice comisionul cu 10% din salariu pentru angajații care au peste 18 ani vechime în companie.
-
-SET serveroutput ON;
-
-DECLARE
-    CURSOR c_angajati IS
-        SELECT 
-           d.department_name, 
-           e.employee_id, 
-           e.commission_pct, 
-           e.hire_date,
-           e.first_name || ' ' || e.last_name AS nume,
-           e.salary
-        FROM
-            employees e
-            INNER JOIN departments d
-            ON e.department_id = d.department_id
-        FOR UPDATE OF commission_pct;
-    angajat c_angajati%ROWTYPE;
-    comision_nou NUMBER DEFAULT 0;
-
-BEGIN
-    OPEN c_angajati;
-    
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
-        IF add_months(angajat.hire_date, 216) < SYSDATE THEN
-            comision_nou := nvl(angajat.commission_pct, 0) + 0.1;
-            UPDATE employees SET commission_pct = comision_nou
-            WHERE CURRENT OF c_angajati;
-        END IF;
-    END LOOP;
-    CLOSE c_angajati;
-END;
+-- Exercitiul 2
+-- tratarea exceptiei de la exercitiul 1
+set serveroutput on
+declare
+    id_dep number;
+    nume_dep departments.department_name%type;
+begin
+    id_dep := &iddep;
+    select department_name into nume_dep from departments where department_id = id_dep;
+    dbms_output.put_line(id_dep||' - '||nume_dep);
+exception
+    when no_data_found then
+        dbms_output.put_line(id_dep||' - N/A');
+end;
 /
 
-SELECT * FROM employees;
+-- Exercitiul 3
+-- inserare in tabela employees a unui nou angajat dara sa aiba employye_id
+set serveroutput on;
+declare
+    insert_angajat exception;
+    pragma exception_init(insert_angajat, -01400);
+begin
+    insert into employees(first_name, last_name, hire_date, salary)
+    values ('Ion', 'Ionescu', '14-aug-2020', 1450);
+exception
+    when insert_angajat then
+        dbms_output.put_line('Nu se accepta inregistrari noi in tabela employees fara employee_id');
+        dbms_output.put_line('Cod eroare '||SQLCODE);
+        dbms_output.put_line('Mesaj '||SQLERRM);
+end;
+/
 
+-- Exercitiul 4
+-- exercitiul 3 dar cu tratare exceptie ce nu a fost declarata
 
--- Ex. 10 Să se facă o listă cu angajații care fac parte dintr-un departament specificat, au o anumită funcție și au venit în companie la o anumită dată specificată. Aceste condiții să fie transmise ca parametri unui cursor.
+set serveroutput on;
+declare
+    insert_angajat exception;
+    pragma exception_init(insert_angajat, -01400);
+begin
+    insert into employees(first_name, last_name, hire_date, salary)
+    values('Ion', 'Ionescu', '14-aug-2020', 1450);
+exception
+    when insert_angajat then
+        dbms_output.put_line('Nu se acceota inserari noi in tabela employees fara employee_id');
+        dbms_output.put_line('Cod eroare '||SQLCODE);
+        dbms_output.put_line('Mesaj '||SQLERRM);
+    when exceptie_eronata then null;
+end;
+/
 
-SET serveroutput ON;
-DECLARE
-    CURSOR c_angajati(departamentId NUMBER, jobID CHAR, hireDate DATE) IS
-        SELECT
-            department_id,
-            first_name || ' ' || last_name AS nume,
-            job_id,
-            hire_date
-        FROM
-            employees
-        WHERE
-            department_id = departamentId AND
-            lower(job_id) = lower(jobID) AND
-            hire_date > hireDate;
-    angajat c_angajati%ROWTYPE;
+-- Exercitiul 5
+/*
+pentru angajatii dintr-un departament, al carui id este de la tastatura, care au un comision null, sa se genereze o exceptie definita
+*/
+set serveroutput on;
+declare
+    idDepartament number;
+    contor number := 100;
+    numeAngajat employees.first_name%type;
+    comision employees.commission_pct%type;
+    dataAngajare employees.hire_date%type;
+    lipsaComision exception;
+begin
+    idDepartament := &deptId;
+    dbms_output.put_line(rpad('Nume', 15,' ')||rpad('Data_ang',20,' ')||lpad('Comision',15,' '));
+    dbms_output.put_line(rpad('=', 15,'=')||rpad('=',20,'=')||lpad('=',15,'='));
+    loop
+        contor := contor + 1;
+        begin
+            select
+                first_name || ' ' || last_name,
+                hire_date,
+                commission_pct
+            into
+                numeAngajat, dataAngajare, comision
+            from
+                employees
+            where
+                department_id = idDepartament
+            and
+                employee_id = contor;
+            if comision is null then
+                raise lipsaComision;
+            end if;
+            dbms_output.put_line(rpad(numeAngajat, 15, ' ')||rpad(dataAngajare, 20, ' ')||lpad(comision, 15, ' '));
+        exception
+            when lipsaComision then
+                dbms_output.put_line(rpad(numeAngajat, 15, ' ')||rpad(dataAngajare, 20, ' ')||lpad('lipsa comision', 15, ' '));
+            when no_data_found then null;
+            when others then null;
+        end;
+        exit when contor = 300;
+    end loop;
+end;
+/
 
-BEGIN
-    dbms_output.put_line('Cursor 1');
-    OPEN c_angajati(20, 'MK_REP', '1-JUN-02');
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
+-- Exercitiul 6
+-- tratare exceptie de mai devreme in blocul principal
+set serveroutput on;
+declare
+    idDepartament number;
+    contor number := 100;
+    numeAngajat employees.first_name%type;
+    comision employees.commission_pct%type;
+    dataAngajare employees.hire_date%type;
+    lipsaComision exception;
+begin
+    idDepartament := &deptId;
+    dbms_output.put_line(rpad('nume',15,' ')||rpad('data_ang',20,' ')||lpad('comision',15,' '));
+    dbms_output.put_line(rpad('=',15,'=')||rpad('=',20,'=')||lpad('=',15,'='));
+    loop
+        contor := contor + 1;
+        begin
+            select
+                first_name || ' ' || last_name,
+                hire_date,
+                commission_pct
+            into
+                numeAngajat,
+                dataAngajare,
+                comision
+            from
+                employees
+            where
+                department_id = idDepartament
+            and
+                employee_id = contor;
+            if comision is null then
+                raise lipsaComision;
+            end if;
+            dbms_output.put_line(rpad(numeAngajat,15,' ')||rpad(dataAngajare,20,' ')||lpad(comision,15,' '));
+        exception
+            when no_data_found then null;
+        end;
+        exit when contor = 300;
+    end loop;
+exception
+    when lipsaComision then
+        dbms_output.put_line(rpad(numeAngajat,15,' ')||rpad(dataAngajare,20,' ')||lpad('lipsa comision',15,' '));
+end;
+/
 
-        dbms_output.put_line(
-            rpad(angajat.department_id, 10) ||
-            rpad(angajat.nume, 30) ||
-            rpad(angajat.job_id, 15) ||
-            rpad(angajat.hire_date, 20)
-        );
-    END LOOP;
-    CLOSE c_angajati;
+-- Exercitiul 7
+/*
+pentru un angajat care nu are comision, sa se defineasca o exceptie ce e tratata in momentul generarii si are codul -20100
+*/
+sset serveroutput on;
+declare
+    id employees.employee_id%type;
+    comision employees.commission_pct%type;
+    lipsaComision exception;
+    pragma exception_init(lipsaComision, -20100);
+begin
+    id := &id;
+    select
+        commission_pct
+    into
+        comision
+    from
+        employees
+    where employee_id = id;
+    if comision is null then
+        raise_application_error(-20100, 'Ang nu are comision!');
+    end if;
+exception
+    when lipsaComision then
+        dbms_output.put_line('Ang nu are comision');
+        dbms_output.put_line('Cod err: '||SQLCODE);
+        dbms_output.put_line('Msg: '||SQLERRM);
+end;
+/
 
-    dbms_output.put_line('Cursor 2');
-    OPEN c_angajati(departamentId => 30, jobId => 'PU_CLERK', hireDate => '1-JUN-02');
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
+-- Exercitiul 8
+-- suma veniturilor dintr-un departament pe o anumita functie
+set serveroutput on;
+declare
+    iddep departments.department_id%type;
+    numedep departments.department_name%type;
+    salariu employees.salary%type;
+    functie employees.job_id%type;
+begin
+    iddep := &iddep;
+    functie := '&functie';
+    select
+        sum(salary + salary * nvl(commission_pct, 0))
+    into
+        salariu
+    from
+        employees
+    where
+        department_id = iddep
+    and
+        upper(job_id) = upper(functie)
+    group by
+        department_id;
+    
+    select
+        department_name
+    into
+        numedep
+    from
+        departments
+    where
+        department_id = iddep;
 
-        dbms_output.put_line(
-            rpad(angajat.department_id, 10) ||
-            rpad(angajat.nume, 30) ||
-            rpad(angajat.job_id, 15) ||
-            rpad(angajat.hire_date, 20)
-        );
-    END LOOP;
-    CLOSE c_angajati;
+    dbms_output.put_line('Suma veniturilor din departamentul '||numedep||' este '||to_char(salariu));
+exception
+    when no_data_found then
+        dbms_output.put_line('In departamentul '||numedep||' nu exista angajati cu functia '||functie);
+end;
+/
 
-    dbms_output.put_line('Cursor 3');
-    OPEN c_angajati(jobId => 'MK_REP', hireDate => '1-JUN-02', departamentId => 20);
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
+-- sau
+set serveroutput on;
+declare
+   iddep departments.department_id%type;
+   numedep departments.department_name%type;
+   salariu employees.salary%type;
+   functie employees.job_id%type;
+begin
+   iddep := &iddep;
+   functie := '&functie';
+   select
+       sum(salary + salary * nvl(commission_pct, 0))
+   into
+       salariu
+   from
+       employees
+   where
+       department_id = iddep
+   and
+       upper(job_id) = upper(functie);
+   
+   select
+       department_name
+   into
+       numedep
+   from
+       departments
+   where
+       department_id = iddep;
 
-        dbms_output.put_line(
-            rpad(angajat.department_id, 10) ||
-            rpad(angajat.nume, 30) ||
-            rpad(angajat.job_id, 15) ||
-            rpad(angajat.hire_date, 20)
-        );
-    END LOOP;
-    CLOSE c_angajati;
+   dbms_output.put_line('Suma veniturilor din departamentul '||numedep||' este '||to_char(salariu));
+exception
+   when no_data_found then
+       dbms_output.put_line('In departamentul '||numedep||' nu exista angajati cu functia '||functie);
+end;
+/
 
-    dbms_output.put_line('Cursor 4');
-    OPEN c_angajati(30, jobId => 'PU_CLERK', hireDate => '1-JUN-02');
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
-
-        dbms_output.put_line(
-            rpad(angajat.department_id, 10) ||
-            rpad(angajat.nume, 30) ||
-            rpad(angajat.job_id, 15) ||
-            rpad(angajat.hire_date, 20)
-        );
-    END LOOP;
-    CLOSE c_angajati;
-
-    dbms_output.put_line('Cursor 5');
-    OPEN c_angajati(30, 'PU_CLERK', hireDate => '1-JUN-02');
-    LOOP
-        FETCH c_angajati INTO angajat;
-        EXIT WHEN c_angajati%NOTFOUND;
-
-        dbms_output.put_line(
-            rpad(angajat.department_id, 10) ||
-            rpad(angajat.nume, 30) ||
-            rpad(angajat.job_id, 15) ||
-            rpad(angajat.hire_date, 20)
-        );
-    END LOOP;
-    CLOSE c_angajati;
-
-END;
-/    
-
+-- de la Maria:
 
 -- Ex. BONUS
 -- Sa se scrie un bloc PL/SQL care selecteaza printr-o colectie urmatoarele informatii:
@@ -245,6 +311,7 @@ END;
 
 
 -- Ex. BONUS 2
+-- TODO
 -- Sa se scrie un bloc PL/SQL care printeaza angajatii care detin venitul maxim din fiecare dep,
 -- angajatii care detin venitul minim din fiecare departament
 --- si pe cei care au peste media departamentului minim 25 %
@@ -268,6 +335,3 @@ DECLARE
     venit_min NUMBER;
     venit_med NUMBER;
     venit_min_25 NUMBER;
-
-BEGIN
-    
