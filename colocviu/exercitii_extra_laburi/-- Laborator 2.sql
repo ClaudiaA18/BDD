4902c9e0b98714c20fd1087359edf8841a553f92
Maria Teodora Popescu <popescumariateodora@yahoo.com>
2024-12-02 04:51:39 -0800

    updated lab2 extra

--- colocviu/exercitii_extra_laburi/-- Laborator 2.sql
+++ colocviu/exercitii_extra_laburi/-- Laborator 2.sql
@@ -18,6 +18,14 @@ BEGIN
         employees; 
 
     FOR contor in angajati.first .. angajati.last LOOP
+    -- deci: luam fiecare angajat pe care l-am pus in tabel
+    -- apoi selectez din job history numai pe angajatii care au ca id pe cei care sunt si in tabelul meu 
+    -- si gen iau dublurile, dar iau numai o data
+    -- ce face ai exact select 1 de mai jos:
+    -- select 1 testeaza existenta unor date sau pentru a seta date in proceduri fara sa le acceseze direct 
+    -- punem from pentruca ne trebuie conditia din where
+    -- avem mai multe schimbari de job si gen nu luam duplicate
+   
         BEGIN
             select 1 
             into
@@ -40,10 +48,10 @@ End;
 /
 
 select
-    1 as ceva_altceva
-    , 'ana are mere' as altceva_ceva2
+    1 as ceva_altceva -- dupa cum se poate observa, intoarce numai 1
+    , 'ana are mere' as altceva_ceva2 -- intoarce asta
 from
-    employees
+    employees -- de atatea ori cate inregistrari sunt aici, cred
 fetch
     first 5 rows only;
-    -- chestia de mai sus iti scrie ana are mere de 5 ori? 
+    -- chestia de mai sus iti scrie ana are mere de 5 ori (dar noi vrem numai de 5 ori)
